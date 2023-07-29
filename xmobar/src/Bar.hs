4b03b3e19cdb2b45cab66d37d3fb52de3a5b7bf7
mislavzanic <mislavzanic3@gmail.com>
2023-07-29 12:38:27 +0200

    fix

--- xmobar/src/Bar.hs
+++ xmobar/src/Bar.hs
@@ -10,7 +10,7 @@ import qualified Hosts.Mzanic as Mzanic
 import Config.BarConfig
 
 myConfig :: Int -> String -> String -> Config
-myConfig n host pos = (hostConfig host) . ( baseConfig n pos ) $
+myConfig n host pos = (hostConfig host) $ ( baseConfig n pos )
   { commands        = myCommands n host pos
   , template        = myTemplate n host pos
   }
